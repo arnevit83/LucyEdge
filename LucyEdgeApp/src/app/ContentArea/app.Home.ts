d0c21c745da1c41d2b432ca846e396a6d0e2a423
duncan <arnevit@msn.com>
2018-12-13 22:22:01 +0000

    Step 3

---
+++ LucyEdgeApp/src/app/ContentArea/app.Home.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Home',
+  templateUrl: './Home.html',
+})
+export class HomeComponent { }
+
--- LucyEdgeApp/src/app/app-routing.module.ts
+++ LucyEdgeApp/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './Header/app.Header';
+import { HomeComponent } from './ContentArea/app.Home';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component:  HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
