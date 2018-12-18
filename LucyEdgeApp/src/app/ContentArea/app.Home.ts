58fb454dd4872265264d10b3c1319c5c32a20447
duncan <arnevit@msn.com>
2018-12-18 23:13:31 +0000

    Homepage Maybe

--- LucyEdgeApp/src/app/ContentArea/app.Home.ts
+++ LucyEdgeApp/src/app/ContentArea/app.Home.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { Title }     from '@angular/platform-browser';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faSkype } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'Home',
@@ -8,5 +13,9 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent {
     faChevronDown = faChevronDown;
+    faPhone = faPhone;
+    faskype = faSkype;
+    faEnvelope = faEnvelope;
+
     public constructor(private titleService: Title ) {   this.titleService.setTitle( "Brand Strategist + Freelance Copywriter + SEO Copywriting" );}
 }
