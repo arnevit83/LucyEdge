1010ad26c4489bfa04534e2fe21f556ba288fb50
duncan <arnevit@msn.com>
2018-12-24 21:31:39 +0000

    Testimonials

--- LucyEdgeApp/src/app/ContentArea/app.Testimonials.ts
+++ LucyEdgeApp/src/app/ContentArea/app.Testimonials.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { Title }     from '@angular/platform-browser';
 import { Meta }     from '@angular/platform-browser';
 
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'Testimonials',
   templateUrl: './Testimonials.html',
 })
 export class TestimonialsComponent {
+  faQuoteLeft = faQuoteLeft;
+  faQuoteRight = faQuoteRight;
   public constructor(private titleService: Title, private meta: Meta  ) {
      this.titleService.setTitle( "Testimonials - Brand Strategist + Freelance Copywriter + SEO Copywriting" );
      this.meta.updateTag({ name: 'description', content: 'I am a Brand Strategist and Freelance Copywriter with SEO Copywriting qualifications. I write on point brand stories to engage customers and convert to sales.' });
