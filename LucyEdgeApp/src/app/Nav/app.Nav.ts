75600e751c9af913a58db170af2ae70de86872ea
duncan <arnevit@msn.com>
2019-01-01 18:39:38 +0000

    Updated Images on skills to cards

--- LucyEdgeApp/src/app/Nav/app.Nav.ts
+++ LucyEdgeApp/src/app/Nav/app.Nav.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faSkype } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'nav',
   templateUrl: './Nav.html',
 })
-export class NavComponent {}
+export class NavComponent {
+  faPhone = faPhone;
+  faskype = faSkype;
+  faEnvelope = faEnvelope;
+  falinkedin = faLinkedin;
+}
