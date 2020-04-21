a693f1d44fa68928fc540e50edb595dc14449787
arnevit83 <33792908+arnevit83@users.noreply.github.com>
2020-04-21 16:17:04 +0100

    Yoga Teacher update

--- LucyEdgeApp/src/app/ContentArea/app.YogaTeachers.ts
+++ LucyEdgeApp/src/app/ContentArea/app.YogaTeachers.ts
@@ -16,6 +16,6 @@ export class YogaTeachersComponent {
   public constructor(private titleService: Title, private meta: Meta ) {
     this.titleService.setTitle( "How to market yourself as a yoga teacher, or yoga studio" );
     this.meta.updateTag({ name: 'description', content: 'Uncomfortable with marketing yourself as a yoga teacher? Learn to love marketing your yoga classes, differentiate yourself, and find students. Discover the yoga marketing strategies that really work with Lucy Edge, yoga advocate, web copywriter, SEO copywriter, brand strategist & author of Yoga School Dropout.' });
-    this.meta.updateTag({ name: 'keywords', content: 'How to market yourself as a yoga teacher,How to market yourself a yoga studio,Yoga marketing strategies,Marketing yoga classes,Marketing for yoga teachers,Marketing yoga,Marketing yoga studio,Yoga marketing,Yoga marketing agency,Yoga marketing ideas,Web copywriter,SEO copywriter,Brand strategist' });
+    this.meta.updateTag({ name: 'keywords', content: 'Yoga market research,Yoga content writer,How to market a yoga studio,Marketing yoga classes,Yoga marketing strategies,How to market yourself as a yoga instructor,Yoga studio marketing plan,How to market a yoga retreat,Marketing for yoga teachers,Yoga marketing agency' });
   }
 }
